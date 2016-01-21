39e28de45dadb707c4013d3e72edea3d6ff5da90
Sawant Shah <sawant@sawantshah.com>
2016-01-21 10:40:48 +0500

    Process link shortcut submission through post.

--- app/routes.php
+++ app/routes.php
@@ -15,3 +15,44 @@ Route::get('/', function()
 {
 	return View::make('form');
 });
+
+Route::post('/',function(){
+    // Form validation rules
+    $rules = array(
+        'link' => 'required|url'
+    );
+
+    $validation = Validator::make(Input::all(), $rules);
+
+    // If validation fails, return to the main page with an error
+    if($validation->fails()) {
+        return Redirect::to('/')
+            ->withInput()
+            ->withErrors($validation);
+    } else {
+    // Now let's check if we already have the link in our database. If so, we get the first result
+        $link = Link::where('url', '=', Input::get('link'))->first();
+
+        // If URL already in database, provide that information back to view
+        if($link) {
+            return Redirect::to('/')
+                ->withInput()
+                ->with('link', $link->shortcut);
+        // Else we create a new unique URL
+        } else {
+            //First we create a new unique Hash
+            do {
+                $newShortcut = Str::random(6);
+            } while(Link::where('shortcut', '=', $newShortcut)->count() > 0);
+
+            //Now we create a new database record
+            Link::create(array('url' => Input::get('link'),'hash' => $newHash));
+
+            //And then we return the new shortened URL info to our action
+            return Redirect::to('/')
+                ->withInput()
+                ->with('link',$newHash);
+            }
+        }
+    }
+);
--- app/views/form.blade.php
+++ app/views/form.blade.php
@@ -7,6 +7,9 @@
         {{-- Form::label('link', 'Link:') --}}
         {{ Form::text('link', Input::old('link'), array('placeholder' => 'Insert URL to be shortened')) }}
 
+        @if(Session::has('errors'))
+            <h3 class="error">{{$errors->first('link')}}</h3>
+        @endif
     {{ Form::close() }}
 
 @stop
